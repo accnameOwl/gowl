340e2562f997e5b7f69c03dad9601e4023c93abb
Tor-Arne <Torarnefe94@gmail.com>
2020-08-26 11:10:43 +0200

    Changes to main.go
    Corresponding to Configs

--- cmd/gowl-server/main.go
+++ cmd/gowl-server/main.go
@@ -1,14 +1,22 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gofiber/fiber"
+	"log"
 )
 
 func main() {
+
+	await := <-ReadEnvFromYaml()
+
 	app := fiber.New()
 	app.Get("/", func(c *fiber.Ctx) {
 		c.Send("Send to client")
 	})
 
-	app.Listen(3000)
+	// Concurrent listen function
+	go func() {
+		log.Fatal(app.Listen(3000))
+	}()
 }
