bfd2a5ec36ca69028b73cd0614b3575c24e78beb
Tor-Arne <Torarnefe94@gmail.com>
2020-08-26 12:04:46 +0200

    ready for work

--- cmd/gowl-server/router/router.go
+++ cmd/gowl-server/router/router.go
@@ -1,28 +1,3 @@
 package router
 
-import (
-	"net/http"
-	"text/template"
-
-	"github.com/julienschmidt/httprouter"
-)
-
-/* var responseTemplate *template.Template
-
-func init() {
-	// set default path
-	responseTemplate = template.Must(template.ParseGlob("./public/*.html"))
-}
-
-// Home ...
-// redirect to home.html
-func Home(res http.ResponseWriter, req *http.Request, param httprouter.Params) {
-	responseTemplate.ExecuteTemplate(res, "index.html", nil)
-}
-
-// About ...
-// redirect to about.html
-func About(res http.ResponseWriter, req *http.Request, param httprouter.Params) {
-	responseTemplate.ExecuteTemplate(res, "about.html", nil)
-}
-*/
+// Gets and functions go here.
--- cmd/gowl-server/router/serve.go
+++ cmd/gowl-server/router/serve.go
@@ -9,12 +9,14 @@ import (
 
 // NewStatic ...
 // Attach new static to a fiber.app
+//
+// params: standard,
 func NewStatic(app *fiber.App, prefix string, dir string, params string) {
 	if prefix == "" || dir == "" {
 		fmt.Println("Could not find prefix or dir argument")
 	} else {
 		if len(params) > 0 {
-			strList := strings.Split(params, " ")
+			strList := strings.Split(params, ", ")
 			for _, v := range strList {
 				if v == "standard" {
 					InitStandardStatics(app)
